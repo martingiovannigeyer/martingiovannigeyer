3eca78747e7611ec10805df97e4254e0cc345ee6
martingiovannigeyer <82170527+martingiovannigeyer@users.noreply.github.com>
2021-04-13 10:55:26 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,8 @@
 let mouseCursor = document.querySelector(".cursor");
 let navLinks = document.querySelectorAll(".navbar li ");
-let headerMain = document.querySelectorAll(" h1");
-let captions = document.querySelectorAll(".sectionheader h3");
-let subheaders = document.querySelectorAll(".centralheader h4");
-let descriptions = document.querySelectorAll(".sectionheader p");
-let images = document.querySelectorAll(".images img");
-let cards = document.querySelectorAll(".card__inner");
+let title = document.querySelectorAll("h1");
+let captions = document.querySelectorAll("h3");
+let descriptions = document.querySelectorAll(".bullet p");
 
 window.addEventListener("mousemove", cursor);
 
@@ -16,70 +13,58 @@ function cursor(e) {
 
 navLinks.forEach(link => {
   link.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("link-grow");
+    mouseCursor.classList.remove("link-grow-fill");
     link.classList.remove("hovered-link");
   });
   link.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("link-grow");
+    mouseCursor.classList.add("link-grow-fill");
     link.classList.add("hovered-link");
   });
 });
 
-headerMain.forEach(link => {
+title.forEach(link => {
   link.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("link-grow");
+    mouseCursor.classList.remove("link-grow-fill");
     link.classList.remove("hovered-link");
   });
   link.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("link-grow");
+    mouseCursor.classList.add("link-grow-fill");
     link.classList.add("hovered-link");
   });
 });
 
-captions.forEach(caption => {
+ captions.forEach(caption => {
   caption.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("link-grow");
-    caption.classList.remove("hovered-caption");
   });
   caption.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
-    caption.classList.add("hovered-caption");
-  });
-});
-
-subheaders.forEach(subheader => {
-  subheader.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("link-grow");
-    subheader.classList.remove("hovered-caption");
-  });
-  subheader.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("link-grow");
-    subheader.classList.add("hovered-caption");
   });
 });
 
 descriptions.forEach(description => {
   description.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("link-grow");
-    description.classList.remove("hovered-caption");
   });
   description.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
-    description.classList.add("hovered-caption");
   });
 });
 
-images.forEach(image => {
-  image.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("link-grow");
-  });
-  image.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("link-grow");
-  });
-});
+// Flipping cards
+let cards = document.querySelectorAll(".card__inner");
 
 cards.forEach(card => {
   card.addEventListener("click", function (e) {
     card.classList.toggle('is-flipped');
   });
 })
+
+// Opening and closing mobile navigation pane
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
